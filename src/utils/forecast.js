840066aa939bedb921f5aa5af412e1bb0e7e455f
Eelco Meijnen <e.meijnen@emakina.nl>
2021-03-14 22:01:51 +0100

    add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,9 @@ const url = 'http://api.weatherstack.com/current?access_key=5bd47abe8726cba84d60
         const temperature = body.current.temperature
         const feelsLike = body.current.feelslike
         const foreCast = body.current.weather_descriptions[0]
-        callback(undefined, foreCast + ' and it is currently ' + temperature + ' but it feels like ' + feelsLike)
+        const humidity = body.current.humidity
+        callback(undefined, foreCast + ' and it\'s currently ' + temperature + ' degrees but it feels like ' + feelsLike + ' degrees, humidity is about ' + humidity + '%')
+
         }
     })
 }
